0568190977083b34a379eb17d61b8700cc193c75
Giuseppe-Bianc <75339264+Giuseppe-Bianc@users.noreply.github.com>
2022-07-17 21:36:06 +0200

    la finestra è sullo schermo

--- GameEngine/main.cpp
+++ GameEngine/main.cpp
@@ -3,9 +3,21 @@
 #include<GLFW/glfw3.h>
 
 int main() {
+	if (!glfwInit()){
+		std::cerr << "glfw failed to init" << std::endl;
+		return 1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	auto window = glfwCreateWindow(800, 600, "game", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+
 
 	std::cout << "Hello Word!" << std::endl;
 
+	glfwTerminate();
 	system("pause");
 	return 0;
 }
