f6753efb15f397b49313cb6cdbf0fabb9a0cc070
Giuseppe-Bianc <75339264+Giuseppe-Bianc@users.noreply.github.com>
2022-07-18 09:40:04 +0200

    aggiunta documentazione

--- GameEngine/utilsLoc.h
+++ GameEngine/utilsLoc.h
@@ -3,12 +3,22 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
+/// <summary> a simple struct for easy delete </summary>
 struct dataTD {
 	GLFWwindow* window;
 };
 
+/// <summary> sets the opengl version to use and the profile </summary>
+/// <param name="maj">the magior version</param>
+/// <param name="min">the minor version</param>
 void setOpenGlVersion(int maj, int min);
 
+/// <summary> it's a siple function to display a error message end reurn the return code; </summary>
+/// <param name="ret">the return code</param>
+/// <param name="msg">the error message</param>
+/// <returns>int</returns>
 int retWhMsg(int ret, std::string const& msg);
 
+/// <summary> delete the data and terminate glfw</summary>
+/// <param name="data">the data to delete</param>
 void destroyAndTerm(dataTD data);
